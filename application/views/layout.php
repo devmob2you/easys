c63315579fa69aa16bfcfdc5ab3ecd37882f2c00
Icaro William <dev@mob2you.com.br>
2013-12-23 12:26:36 -0200

    Start Project

---
+++ application/views/layout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>EaSyS ERP v1.0</title>
+
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/general.css'); ?>" />
+<script src="<?php echo base_url('public/js/jquery.js'); ?>"></script>
+	
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/bootstrap.css'); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/bootstrap-responsive.css'); ?>" />
+<script src="<?php echo base_url('public/js/bootstrap.js'); ?>"></script>
+
+</head>
+<body>
+	<div class="container" style="background:#ccc;">
+		
+        <p>Hello!</p>
+        
+	</div>
+</body>
+</html>
